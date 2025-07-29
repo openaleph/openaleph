7dbba63bf7c75b263bbf4f16288ce33d73ac4256
Simon Wörpel <simon.woerpel@pm.me>
2025-07-29 13:21:53 +0200

    👽️ (aggregator) dataset -> fragments

--- aleph/logic/aggregator.py
+++ aleph/logic/aggregator.py
@@ -1,5 +1,5 @@
-from ftmq.store.fragments import get_dataset
-from ftmq.store.fragments.dataset import Dataset
+from ftmq.store.fragments import get_fragments
+from ftmq.store.fragments.dataset import Fragments
 
 MODEL_ORIGIN = "model"
 
@@ -8,7 +8,7 @@ def get_aggregator_name(collection) -> str:
     return "collection_%s" % collection.id
 
 
-def get_aggregator(collection, origin="aleph") -> Dataset:
+def get_aggregator(collection, origin="aleph") -> Fragments:
     """Connect to a followthemoney dataset."""
     dataset = get_aggregator_name(collection)
-    return get_dataset(dataset, origin=origin)
+    return get_fragments(dataset, origin=origin)
