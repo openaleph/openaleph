2e1fbbc21787070b159cba6ac760145540291066
Alex Ștefănescu <alex.stefanescu@pm.me>
2025-07-31 17:24:10 +0200

    Process procrastinate job status in status API

--- aleph/views/status_api.py
+++ aleph/views/status_api.py
@@ -1,13 +1,20 @@
 import logging
+
 from flask import Blueprint, request
+from openaleph_procrastinate.status import get_status as get_job_status
 
 from aleph.model import Collection
-from aleph.queues import get_active_dataset_status, get_dataset_collection_id
 from aleph.views.serializers import CollectionSerializer
 from aleph.views.util import jsonify, require
 
 log = logging.getLogger(__name__)
 blueprint = Blueprint("status_api", __name__)
+
+FINISHED_JOBS = ["succeeded"]
+RUNNING_JOBS = ["doing"]
+PENDING_JOBS = ["todo"]
+FAILED_JOBS = ["failed"]
+CANCELLED_JOBS = ["aborted", "aborting", "cancelled"]
 
 
 @blueprint.route("/api/2/status", methods=["GET"])
@@ -30,20 +37,73 @@ def status():
     """
     require(request.authz.logged_in)
     request.rate_limit = None
-    status = get_active_dataset_status()
-    datasets = status.pop("datasets", {})
-    collections = (get_dataset_collection_id(d) for d in datasets.keys())
-    collections = (c for c in collections if c is not None)
-    collections = Collection.all_by_ids(collections, deleted=True).all()
-    collections = {c.id: c for c in collections}
-    serializer = CollectionSerializer(nested=True)
+
     results = []
-    for dataset, status in sorted(datasets.items()):
-        collection_id = get_dataset_collection_id(dataset)
+    # get the job status from openaleph_procrastinate
+    jobs_status = get_job_status()
+    # get serialized collection metadata from ftm_store
+    serializer = CollectionSerializer(nested=True)
+
+    for collection_name in jobs_status:
+        # periodic tasks don't have a collection_id
+        if not collection_name:
+            continue
+
+        collection_status = {}
+
+        # if the current user can read the current collection, add metadata
+        collection_id = int(collection_name.split("_")[-1])
         if not request.authz.can(collection_id, request.authz.READ):
             continue
-        collection = collections.get(collection_id)
-        if collection is not None:
-            status["collection"] = serializer.serialize(collection.to_dict())
-        results.append(status)
+        collection_obj = Collection.by_id(collection_id, deleted=True)
+        if collection_obj is not None:
+            collection_status["collection"] = serializer.serialize(
+                collection_obj.to_dict()
+            )
+
+        # convert the status from openaleph_procrastinate to the structure of the /status API
+        stages = {}
+        for jobs_status_per_stage in jobs_status[collection_name]:
+            stage = jobs_status_per_stage["stage"]
+            finished_jobs = (
+                jobs_status_per_stage["number_of_jobs"]
+                if jobs_status_per_stage["status_of_jobs"] in FINISHED_JOBS
+                else 0
+            )
+            running_jobs = (
+                jobs_status_per_stage["number_of_jobs"]
+                if jobs_status_per_stage["status_of_jobs"] in RUNNING_JOBS
+                else 0
+            )
+            pending_jobs = (
+                jobs_status_per_stage["number_of_jobs"]
+                if jobs_status_per_stage["status_of_jobs"] in PENDING_JOBS
+                else 0
+            )
+            if stage in stages:
+                stages[stage]["pending"] += pending_jobs
+                stages[stage]["running"] += running_jobs
+                stages[stage]["finished"] += finished_jobs
+            else:
+                stages[stage] = {
+                    "job_id": "",
+                    "stage": stage,
+                    "pending": pending_jobs,
+                    "running": running_jobs,
+                    "finished": finished_jobs,
+                }
+
+        collection_status["jobs"] = [stages[stage] for stage in stages]
+        collection_status["finished"] = sum(
+            [stages[stage]["finished"] for stage in stages]
+        )
+        collection_status["running"] = sum(
+            [stages[stage]["running"] for stage in stages]
+        )
+        collection_status["pending"] = sum(
+            [stages[stage]["pending"] for stage in stages]
+        )
+
+        results.append(collection_status)
+
     return jsonify({"results": results, "total": len(results)})
