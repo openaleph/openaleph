6e00b70a3b49d69f6ba0e0379717353e27f2de5f
Simon Wörpel <simon.woerpel@pm.me>
2025-08-03 02:25:06 +0200

    🧑‍💻 (migrate) Initialize procrastinate db

--- aleph/migration.py
+++ aleph/migration.py
@@ -1,5 +1,6 @@
 import flask_migrate
-from sqlalchemy import MetaData, inspect, text
+from openaleph_procrastinate.app import init_db
+from sqlalchemy import MetaData, inspect
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.exc import InternalError
 
@@ -13,6 +14,7 @@ def upgrade_system():
     archive.upgrade()
     create_system_roles()
     upgrade_search()
+    init_db()
 
 
 def cleanup_deleted():
@@ -30,21 +32,13 @@ def destroy_db():
     metadata.bind = db.engine
     metadata.reflect(db.engine)
     tables = list(metadata.sorted_tables)
-    aleph_tables = [x for x in tables if "procrastinate" not in x.name]
-    while len(aleph_tables):
-        for table in aleph_tables:
+    while len(tables):
+        for table in tables:
             try:
                 table.drop(bind=db.engine, checkfirst=True)
-                aleph_tables.remove(table)
+                tables.remove(table)
             except InternalError:
                 pass
     for enum in inspect(db.engine).get_enums():
         enum = ENUM(name=enum["name"])
-        if "procrastinate" not in enum.name:
-            enum.drop(bind=db.engine, checkfirst=True)
-    procrastinate_tables = [x for x in tables if "procrastinate" in x.name]
-    for table in procrastinate_tables:
-        q = f"TRUNCATE {table.name} RESTART IDENTITY CASCADE;"
-        with db.engine.connect() as conn:
-            conn.execute(text(q))
-            conn.commit()
+        enum.drop(bind=db.engine, checkfirst=True)
