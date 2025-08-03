1a039c6334cc2a786b33ac6094abf6d0c05a7001
Simon Wörpel <simon.woerpel@pm.me>
2025-08-03 12:46:36 +0200

    💚 (tests) Make sure dev requirements are installed (for docker)

--- contrib/test.sh
+++ contrib/test.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-psql -c "DROP DATABASE IF EXISTS aleph;" $ALEPH_DATABASE_URI
-psql -c "CREATE DATABASE aleph;" $ALEPH_DATABASE_URI
-
-opal-procrastinate init-db
-
-pytest aleph/ --cov=aleph --cov-report html --cov-report lcov $@
+pip install -r requirements-dev.txt
+pytest -s aleph/tests $@
