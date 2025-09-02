85791b36901fb05d6fee4fec38a2234e3182def4
Simon Wörpel <simon.woerpel@pm.me>
2025-09-02 21:24:08 +0200

    🍻 ...or this

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,10 @@
 // import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as _ from 'lodash';
 import App from 'app/App';
+
+// Make lodash available globally for MapLibre GL JS
+window._ = _;
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
