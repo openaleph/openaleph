14ea92f95ac309f666f7e6274f5ec0bf8ea72ade
Simon Wörpel <simon.woerpel@pm.me>
2025-10-06 15:36:19 +0200

    🐛 (xref) Disable concurrency for xref indexer

--- aleph/index/xref.py
+++ aleph/index/xref.py
@@ -79,7 +79,7 @@ def _index_form(collection, matches):
 
 def index_matches(collection, matches, sync=False):
     """Index cross-referencing matches."""
-    bulk_actions(_index_form(collection, matches), sync=sync)
+    bulk_actions(_index_form(collection, matches), sync=sync, max_concurrency=1)
 
 
 def iter_matches(collection, authz):
