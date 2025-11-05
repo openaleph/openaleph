24ad75a89a0a9681810d86b4f8e0f3c9de49d934
Simon Wörpel <simon.woerpel@pm.me>
2025-11-05 16:26:33 +0100

    🔖 Bump version: 5.1.0-rc6 → 5.1.0-rc7

--- aleph/__init__.py
+++ aleph/__init__.py
@@ -3,7 +3,7 @@ import warnings
 
 from sqlalchemy.exc import SAWarning
 
-__version__ = "5.1.0-rc6"
+__version__ = "5.1.0-rc7"
 
 # shut up useless SA warning:
 warnings.filterwarnings("ignore", "Unicode type received non-unicode bind param value.")
