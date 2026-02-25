e2ee49e6706b75d7c49910aa56e60124700690c2
Simon Wörpel <simon.woerpel@pm.me>
2026-02-25 11:55:44 +0100

    🔧 Set default nk algorithm to regression-v1

--- aleph/settings.py
+++ aleph/settings.py
@@ -199,7 +199,7 @@ class Settings:
         ###############################################################################
         # XREF Model Selection
         self.XREF_MODEL = env.get("FTM_COMPARE_MODEL", None)
-        self.XREF_ALGORITHM = env.get("NOMENKLATURA_XREF_ALGORITHM", None)
+        self.XREF_ALGORITHM = env.get("NOMENKLATURA_XREF_ALGORITHM", "regression-v1")
 
         ###############################################################################
         # Feature flags
