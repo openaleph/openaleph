d973433b592101418e9c8c93f8aa48c3b98d54fc
Simon Wörpel <simon.woerpel@pm.me>
2026-03-16 10:28:39 +0100

    🐛 (queues) Properly pass through priority

--- aleph/procrastinate/queues.py
+++ aleph/procrastinate/queues.py
@@ -39,7 +39,7 @@ class Context(TypedDict):
     priority: int | None
 
 
-def get_context(collection: Collection) -> Context:
+def get_context(collection: Collection, priority: int | None = None) -> Context:
     """Set some task context variables that configure the ingestors."""
     from aleph.logic.aggregator import get_aggregator_name
 
@@ -48,7 +48,7 @@ def get_context(collection: Collection) -> Context:
             "languages": [x for x in collection.languages if x],
             "ftmstore": get_aggregator_name(collection),
             "namespace": collection.foreign_id,
-            "priority": Priorities.USER if collection.casefile else None,
+            "priority": priority or (Priorities.USER if collection.casefile else None),
         }
     )
 
@@ -61,19 +61,19 @@ def queue_ingest(collection: Collection, proxy: EntityProxy, **context: Any) -> 
 
 
 def queue_analyze(collection: Collection, proxy: EntityProxy, **context: Any) -> None:
-    context = {**context, **get_context(collection)}
+    context = {**context, **get_context(collection, Priorities.USER)}
     dataset = get_aggregator_name(collection)
     with app.open():
-        defer.analyze(app, dataset, [proxy], priority=Priorities.USER, **context)
+        defer.analyze(app, dataset, [proxy], **context)
 
 
 def queue_transcribe(
     collection: Collection, proxy: EntityProxy, **context: Any
 ) -> None:
-    context = {**context, **get_context(collection)}
+    context = {**context, **get_context(collection, Priorities.USER)}
     dataset = get_aggregator_name(collection)
     with app.open():
-        defer.transcribe(app, dataset, [proxy], priority=Priorities.USER, **context)
+        defer.transcribe(app, dataset, [proxy], **context)
 
 
 def queue_translate(
@@ -81,10 +81,10 @@ def queue_translate(
     proxy: EntityProxy,
     **context: Any,
 ) -> None:
-    context = {**context, **get_context(collection)}
+    context = {**context, **get_context(collection, Priorities.USER)}
     dataset = get_aggregator_name(collection)
     with app.open():
-        defer.translate(app, dataset, [proxy], priority=Priorities.USER, **context)
+        defer.translate(app, dataset, [proxy], **context)
     # we want to trace the processing status for the UI:
     tracer = defer.tasks.translate.get_tracer(oa_settings.redis_url)
     tracer.add(proxy.id)
